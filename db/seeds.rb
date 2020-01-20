9bfbb72cf5306996647f15035789b3aaac45eadd
Pierce Franklin <piush_r@hotmail.com>
2020-01-20 11:10:19 -0600

    refined the front end and added a seed record

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,10 +17,39 @@ ingredients_file = open(url).read
 ingredients = JSON.parse(ingredients_file)
 
 # p ingredients[“drinks”][0]
+puts 'Cleaning database...'
+Cocktail.destroy_all
 
+puts 'Creating cocktails...'
+
+file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
+cocktail = Cocktail.new(name: 'NES')
+cocktail.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+# cocktials_attributes = [
+#   { name: 'White Russian' },
+#   { name: 'BELLINI'},
+#   {
+#     name:         'CHAMPAGNE COCKTAIL'
+#   },
+#   {
+#     name:         'IRISH COFFEE'
+#   },
+#   {
+#     name:         'BAMBOO'
+#   },
+#   {
+#     name:         'LONG ISLAND ICED TEA'
+#   }
+# ]
+# Cocktail.create!(cocktials_attributes)
+Cocktail.create(cocktail)
 Ingredient.destroy_all
 
 ingredients['drinks'].each do |ingredient|
   # p ingredient["strIngredient1"]
   Ingredient.create(name: ingredient['strIngredient1'])
 end
+
+# dose_attributes =
+
+puts 'Finished!'
